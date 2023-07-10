14cbe5009ee8f0cab5f4f68a68e9531f795ff673
oflatt <oflatt@gmail.com>
2023-07-10 11:57:40 -0600

    basic read in and send to egglog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::io::{stdin, Read};
+
+use egglog::{ast::parse::ExprParser, ast::Expr, EGraph};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum EggCCError {
+    #[error("Egglog error: {0}")]
+    EggLog(egglog::Error),
+    #[error("Parse error: {0}")]
+    Parse(String),
+}
+
+struct Optimizer {}
+
+impl Optimizer {
+    fn optimize(&self, program: &str) -> Result<Expr, EggCCError> {
+        let expr_parser = ExprParser::new();
+        let egglog_code = self.make_optimizer_for(program);
+        let mut egraph = EGraph::default();
+        egraph
+            .parse_and_run_program(&egglog_code)
+            .map_err(EggCCError::EggLog)?
+            .into_iter()
+            .for_each(|output| log::info!("{}", output));
+        let extract_report = egraph
+            .extract_expr(
+                expr_parser
+                    .parse(program)
+                    .map_err(|err| EggCCError::Parse(err.to_string()))?,
+                0,
+            )
+            .map_err(EggCCError::EggLog)?;
+        Ok(extract_report.expr)
+    }
+
+    fn make_optimizer_for(&self, program: &str) -> String {
+        format!(
+            "
+        (datatype Imp
+        (Int i64)
+        (True)
+        (False)
+        (Add Imp Imp)
+        (Sub Imp Imp)
+        (Mul Imp Imp)
+        (Div Imp Imp))
+        {program}"
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin().lock().read_to_string(&mut input).unwrap();
+    let optimizer = Optimizer {};
+    match optimizer.optimize(&input) {
+        Ok(expr) => println!("{}", expr),
+        Err(err) => println!("{}", err),
+    }
 }

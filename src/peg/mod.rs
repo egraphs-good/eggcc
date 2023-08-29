7681008b5df7364a310f30159b1e6b61dc9df17b
Alex Fischman <alexdfischman@gmail.com>
2023-08-28 17:15:18 -0700

    Define PEG expressions using RVSDG structures

--- src/peg/mod.rs
+++ src/peg/mod.rs
@@ -5,4 +5,19 @@
 //! * ["Equality Saturation: A New Approach to Optimization"](https://arxiv.org/abs/1012.1802)
 //! by Tate, Stepp, Tatlock, and Lerner
 
-use crate::cfg::{ret_id, Annotation, BranchOp, Cfg, CondVal, Identifier};
+use crate::rvsdg::{Expr, Operand};
+
+/// Define PEGs in terms of RVSDG units.
+#[allow(dead_code)]
+pub(crate) enum Peg {
+    PureOp(Expr),
+    Phi {
+        pred: Operand,
+        if_true: Operand,
+        if_false: Operand,
+    },
+    Theta {
+        init: Operand,
+        loop_: Operand,
+    },
+}

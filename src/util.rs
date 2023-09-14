cb98e815489fd1baa367e9577eb9a765fd09ba1e
oflatt <oflatt@gmail.com>
2023-09-14 14:35:36 -0700

    migrate debug viz as well

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,11 @@
-use crate::peg::rvsdg_to_peg;
 use crate::Optimizer;
-use hashbrown::HashMap;
-use petgraph::dot::Dot;
+use std::fmt::Debug;
 use std::{
     ffi::OsStr,
     fmt::{Display, Formatter},
     io,
     path::PathBuf,
+    str::FromStr,
 };
 
 pub(crate) struct ListDisplay<'a, TS>(pub TS, pub &'a str);
@@ -44,144 +43,82 @@ pub(crate) fn parse_from_string(input: &str) -> bril_rs::Program {
     load_program_from_read(json_str.as_bytes())
 }
 
-/// Visual representations of intermediate representations computed on a bril
-/// program.
+/// Write the visualizations to output files in the output directory.
+/// If the directory does not exist, it creates it.
+/// If the directory contains any files whose names conflict with the
+/// output files, it replaces them.
 ///
-/// This struct is intended for use in debugging eggcc. SVG visualizations can
-/// be opened directly in a web browser. Dot visualizations can be converted to
-/// (e.g.) PNGs with utilities like graphviz, e.g.
-/// `dot -Tpng input_cfg.dot -o input_cfg.png`.
-#[allow(unused)]
-pub struct DebugVisualizations {
-    /// The raw CFG before any restructuring, in Dot format,
-    /// for each function in the program.
-    pub(crate) input_cfgs: HashMap<String, String>,
-    /// The restructured CFG ready for RVSDG conversion, in Dot format.
-    pub(crate) restructured_cfgs: HashMap<String, String>,
-    /// The RVSDG, rendered in SVG format.
-    pub(crate) rvsdg: String,
-    /// The PEG, rendered in DOT format.
-    pub(crate) peg: String,
+/// Like other utilities related to `DebugVisualizations`, this method is
+/// only intended for debugging eggcc.
+pub fn visualize(program_path: PathBuf, output_dir: PathBuf) -> io::Result<()> {
+    use std::fs::File;
+    use std::io::Write;
+
+    // make the directory if it doesn't exist
+    if !output_dir.exists() {
+        std::fs::create_dir_all(&output_dir)?;
+    }
+
+    let all_configs = Run::all_configurations_for(program_path);
+
+    let results = all_configs.iter().map(|run| (run, run.run()));
+
+    for (run, result) in results {
+        let mut output_path = output_dir.clone();
+        assert!(output_path.is_dir());
+        output_path.push(format!(
+            "{}{}",
+            run.name(),
+            result.visualization_file_extension
+        ));
+        let mut file = File::create(output_path)?;
+        file.write_all(result.visualization.as_bytes())?;
+
+        if result.visualization_file_extension == ".dot" {
+            let output_path = output_dir.join(format!("{}.svg", run.name()));
+            run_cmd_line(
+                "dot",
+                [
+                    "-Tsvg",
+                    "-o",
+                    &output_path.into_os_string().into_string().unwrap(),
+                ],
+                &result.visualization,
+            )?;
+        }
+    }
+
+    Ok(())
 }
 
-impl DebugVisualizations {
-    /// Compute visualizations for the given bril program.
-    ///
-    /// This function is intended for use in debugging.
-    ///
-    /// # Panics
-    /// Any failures in the conversion to CFG or RVSDG will cause a panic.
-    pub fn new(input: &str) -> DebugVisualizations {
-        let program = parse_from_string(input);
-        let cfg = Optimizer::program_to_cfg(&program);
-        let rvsdg = Optimizer::program_to_rvsdg(&program).unwrap();
-
-        let input_cfgs = cfg
-            .functions
-            .iter()
-            .map(|f| (f.name.clone(), format!("{:#?}", Dot::new(&f.graph))))
-            .collect();
-        let restructured_cfgs = cfg
-            .functions
-            .iter()
-            .map(|f| {
-                let mut f = f.clone();
-                f.restructure();
-                (f.name.clone(), format!("{:#?}", Dot::new(&f.graph)))
-            })
-            .collect();
-
-        DebugVisualizations {
-            input_cfgs,
-            restructured_cfgs,
-            rvsdg: rvsdg.to_svg(),
-            peg: rvsdg_to_peg(&rvsdg).graph(),
-        }
-    }
-
-    /// Write the visualizations to output files in the given directory.
-    /// If the directory does not exist, it creates it.
-    /// If the directory contains any files whose names conflict with the
-    /// output files, it replaces them.
-    ///
-    /// * The `input_cfg` field is written to `input_cfg.dot`.
-    /// * The `restructured_cfg` field is written to `function_name` + `restructured.dot`.
-    /// * The `rvsdg` field is written to `rvsdg.svg`.
-    ///
-    /// Like other utilities related to `DebugVisualizations`, this method is
-    /// only intended for debugging eggcc.
-    pub fn write_output(&self, path: PathBuf) -> io::Result<()> {
-        use std::fs::File;
-        use std::io::Write;
-
-        // make the directory if it doesn't exist
-        if !path.exists() {
-            std::fs::create_dir_all(&path)?;
-        }
-
-        for (name, content) in
-            self.input_cfgs
-                .iter()
-                .map(|(name, content)| (format!("{name}_cfg.dot"), content.clone()))
-                .chain(self.restructured_cfgs.iter().map(|(name, content)| {
-                    (format!("{name}_restructured_cfg.dot"), content.clone())
-                }))
-                .chain([("rvsdg.svg".to_string(), self.rvsdg.clone())])
-                .chain([("peg.dot".to_string(), self.peg.clone())])
-        {
-            let mut output_path = path.clone();
-            assert!(path.is_dir());
-            output_path.push(name.clone());
-            let mut file = File::create(output_path)?;
-            file.write_all(content.as_bytes())?;
-
-            // if it's a dot file, also write
-            // an svg using the dot program:
-            if name.ends_with(".dot") {
-                let output_path = path.join(name.replace(".dot", ".svg"));
-                self.run(
-                    "dot",
-                    [
-                        "-Tsvg",
-                        "-o",
-                        &output_path.into_os_string().into_string().unwrap(),
-                    ],
-                    &content,
-                )?;
-            }
-        }
-        Ok(())
-    }
-
-    /// Invokes some program with the given arguments, piping the given input to the program.
-    /// Returns an error if the program returns a non-zero exit code.
-    /// Code adapted from https://github.com/egraphs-good/egg/blob/e7845c5ae34267256b544c8e6b5bc36d91d096d2/src/dot.rs#L127
-    pub fn run<S1, S2, I>(&self, program: S1, args: I, input: &str) -> std::io::Result<()>
-    where
-        S1: AsRef<OsStr>,
-        S2: AsRef<OsStr>,
-        I: IntoIterator<Item = S2>,
-    {
-        use std::io::Write;
-        use std::process::{Command, Stdio};
-        let mut child = Command::new(program)
-            .args(args)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::null())
-            .spawn()?;
-        let stdin = child.stdin.as_mut().expect("Failed to open stdin");
-        write!(stdin, "{}", input)?;
-        match child.wait()?.code() {
-            Some(0) => Ok(()),
-            Some(e) => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                format!("dot program returned error code {}", e),
-            )),
-            None => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "dot program was killed by a signal",
-            )),
-        }
+/// Invokes some program with the given arguments, piping the given input to the program.
+/// Returns an error if the program returns a non-zero exit code.
+/// Code adapted from https://github.com/egraphs-good/egg/blob/e7845c5ae34267256b544c8e6b5bc36d91d096d2/src/dot.rs#L127
+pub fn run_cmd_line<S1, S2, I>(program: S1, args: I, input: &str) -> std::io::Result<()>
+where
+    S1: AsRef<OsStr>,
+    S2: AsRef<OsStr>,
+    I: IntoIterator<Item = S2>,
+{
+    use std::io::Write;
+    use std::process::{Command, Stdio};
+    let mut child = Command::new(program)
+        .args(args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::null())
+        .spawn()?;
+    let stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    write!(stdin, "{}", input)?;
+    match child.wait()?.code() {
+        Some(0) => Ok(()),
+        Some(e) => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("dot program returned error code {}", e),
+        )),
+        None => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "dot program was killed by a signal",
+        )),
     }
 }
 
@@ -190,6 +127,25 @@ pub enum RunType {
     StructuredConversion,
     RvsdgConversion,
     NaiiveOptimization,
+}
+
+impl Debug for RunType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl FromStr for RunType {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "structured" => Ok(RunType::StructuredConversion),
+            "rvsdg" => Ok(RunType::RvsdgConversion),
+            "naiive" => Ok(RunType::NaiiveOptimization),
+            _ => Err(format!("Unknown run type: {}", s)),
+        }
+    }
 }
 
 impl Display for RunType {

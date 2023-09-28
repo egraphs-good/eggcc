07bfbebbec541b9c02716bec9c2082737d8f9c80
Alex Fischman <alexdfischman@gmail.com>
2023-09-28 15:13:33 -0700

    Add peg-interp flag

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ struct Args {
     #[clap(long)]
     interp: bool,
 
+    /// Translate the input program to a PEG and run it.
+    /// Throws an error if used with the interp flag.
+    #[clap(long)]
+    peg_interp: bool,
+
     /// Path that eggcc will put interp profile results
     #[clap(long)]
     profile_out: Option<PathBuf>,
@@ -47,10 +52,22 @@ fn main() {
         return;
     }
 
-    let run = Run {
-        prog_with_args: TestProgram::File(args.file.clone()).read_program(),
-        test_type: args.run_mode,
-        interp: args.interp,
+    let run = if args.peg_interp {
+        if args.interp {
+            eprintln!("--peg-interp is not compatible with --interp");
+            return;
+        }
+        Run {
+            prog_with_args: TestProgram::File(args.file.clone()).read_program(),
+            test_type: RunType::PegConversion,
+            interp: true,
+        }
+    } else {
+        Run {
+            prog_with_args: TestProgram::File(args.file.clone()).read_program(),
+            test_type: args.run_mode,
+            interp: args.interp,
+        }
     };
 
     let result = run.run();

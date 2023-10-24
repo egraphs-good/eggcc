c09cfa19400aaa7f03a38450719ef3c77510f920
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2023-10-24 12:03:27 -0700

    Failing function inlining

--- src/rvsdg/egglog_optimizer/mod.rs
+++ src/rvsdg/egglog_optimizer/mod.rs
@@ -14,6 +14,7 @@ pub fn rvsdg_egglog_code() -> String {
         constant_fold_egglog(),
         include_str!("gamma_rewrites.egg").to_string(),
         include_str!("loop-optimizations.egg").to_string(),
+        include_str!("function_inline.egg").to_string(),
     ];
     code.join("\n")
 }

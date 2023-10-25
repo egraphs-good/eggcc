37b4e8065c19c842c801768f50bd53add770408b
oflatt <oflatt@gmail.com>
2023-10-25 10:47:42 -0700

    reassoc works

--- src/rvsdg/egglog_optimizer/reassoc.rs
+++ src/rvsdg/egglog_optimizer/reassoc.rs
@@ -6,6 +6,7 @@
 pub(crate) fn reassoc_rules() -> String {
     let mut res = vec![];
     for op in ["badd", "bmul"] {
+        // Move constants to the left using commutativity
         res.push(format!(
             "
      (rule
@@ -14,6 +15,58 @@ pub(crate) fn reassoc_rules() -> String {
       ((union lhs ({op} (IntT) num other))))"
         ))
     }
+
+    // Make all terms have the form (+ a (+ b (+ ...)))
+    // By moving nesting to the right.
+    res.push(
+        "
+      (rule
+        ((= lhs (badd (IntT)
+                      (Node (PureOp (badd (IntT) a b)))
+                      c)))
+        ((union lhs
+                (badd (IntT)
+                      a
+                      (Node (PureOp (badd (IntT) b c)))))))
+      "
+        .to_string(),
+    );
+
+    // Move constants up the tree
+    res.push(
+        "
+        (rule
+          ((= lhs (badd (IntT)
+                        a
+                        (Node (PureOp (badd (IntT) b c)))))
+           (= b (Node (PureOp (Const (IntT) (const) (Num n1)))))            
+          )
+          ((union lhs
+             (badd (IntT)
+                   b
+                   (Node (PureOp (badd (IntT) a c))))))               
+        )
+      "
+        .to_string(),
+    );
+
+    // Constant fold two adjacent constants
+    res.push(
+        "
+      (rule
+        ((= lhs (badd (IntT)
+                      a
+                      (Node (PureOp (badd (IntT) b c)))))
+         (= a (Node (PureOp (Const (IntT) (const) (Num n1)))))
+         (= b (Node (PureOp (Const (IntT) (const) (Num n2))))))
+
+        ((union lhs
+          (badd (IntT)
+            (Node (PureOp (Const (IntT) (const) (Num (+ n1 n2)))))
+            c))))      
+      "
+        .to_string(),
+    );
 
     res.join("\n")
 }

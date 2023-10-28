a64f4ae672469eca46bfe32fc0a1fdbe98a58ac3
Ryan Berger <ryanbberger@gmail.com>
2023-10-28 16:21:03 -0700

    update python docs, include more files to profile

--- infra/aggregate.py
+++ infra/aggregate.py
@@ -4,25 +4,42 @@ from os.path import join
 import json
 
 
+# main will aggregate all profile info into a single json array.
+# It walks a file that looks like:
+# tmp
+# - bench
+# -- <benchmark name>
+# ---- run_method.json
+# ---- run_method.profile
 def main():
     root = "./tmp/bench"
     res = []
 
     for dir in listdir(root):
         dir_path = join(root, dir)
+
         for file_path in listdir(dir_path):
+            # since there is a .profile and a .json file for each bench method
+            # only fire on files with .json, and we'll extrapolate the name of the .profile
             if ".json" not in file_path:
                 continue
 
+            # get the name of the run method as promised above
             run_method = file_path[:-len(".json")]
             result = {"runMethod": run_method, "benchmark": dir}
 
+            # we can use the file_path to get the hyperfine results, store them in result["hyperfine"]
             with open(join(dir_path, file_path)) as f:
                 result["hyperfine"] = json.load(f)
 
+            # we can't use file_path, but we can use the run_method to access the .profile file
+            # that is holding the instruction count. It should be a single line like:
+            # total_dyn_inst: <num insts>
             with open(join(dir_path, f"{run_method}.profile")) as f:
-                inst_count = f.read()
-                result["total_dyn_inst"] = int(inst_count[len("total_dyn_inst: ")])
+                inst_file_str = f.read()
+                # strip the first piece of the string off to get just the number of instructions
+                # casting to an int
+                result["total_dyn_inst"] = int(inst_file_str[len("total_dyn_inst: ")])
 
             res.append(result)
 
--- infra/profile.sh
+++ infra/profile.sh
@@ -9,14 +9,15 @@ cleanup() {
 trap cleanup EXIT
 
 # TODO: take in file glob as command line argument
-PROFILES=tests/small/add.bril
+PROFILES=(tests/small/*.bril)
 
 RUNMODES=("nothing" "rvsdg-optimize")
 
 # create temporary directory structure necessary for bench runs
 mkdir -p ./tmp/bench
 
-# bench will benchmark a single bril file, outputting its contents to ./tmp/bench/<PROFILE_NAME>.json
+# bench will benchmark a single bril file, outputting hyperfine contents to ./tmp/bench/<PROFILE_NAME>.json
+# and will output the number of instructions it executed to ./tmp/bench/<PROFILE_NAME>.profile
 bench() {
     # strip the file path down to just the file name
     # TODO: profile name is not unique, generate a unique output path (it will be aggregated anyway)
@@ -26,7 +27,7 @@ bench() {
     PROFILE_DIR=./tmp/bench/"$PROFILE_NAME"
     mkdir "$PROFILE_DIR"
 
-    # loop over RUNMODES and generate a profile for each, leaving it in the PROFILE_NAME directory
+    # loop over RUNMODES and generate a profile for each, leaving it in PROFILE_DIR
     for mode in "${RUNMODES[@]}"
     do
       echo "profiling $mode"
@@ -39,9 +40,9 @@ bench() {
     done
 }
 
-for p in $PROFILES
+for p in "${PROFILES[@]}"
 do
-  bench $p
+  bench "$p"
 done
 
 # aggregate all profile data into a single JSON array

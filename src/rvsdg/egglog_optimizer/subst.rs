68fa5873109581dbbea56d56b08eb6080ae71f49
Eli Rosenthal <ezrosent@users.noreply.github.com>
2023-10-28 17:22:43 -0700

    Move simultaneous substitutions into pure egglog (#127)
    * Move simultaneous substitutions into pure egglog
    
    This also fixes an issue where the initial "nested" implementation was not
    capture-avoiding.
    
    * remove reassoc-peg snapshot

--- src/rvsdg/egglog_optimizer/subst.rs
+++ src/rvsdg/egglog_optimizer/subst.rs
@@ -1,71 +1,27 @@
 use super::BRIL_OPS;
 
-// TODO handle subst from arbitrary Body
-// instead of just a `VO`
-fn subst_all_rule(btype: String) -> String {
-    format!(
-        "
-  ;;############## Subst{btype}All ##############
-  ;; each operand in the vector corresponds to an argument
-  ;; and is substituted as that argument
-  ;; For example, (SubstExprAll (vec-of (Const 1) (Const 2))
-  ;;                            (Add (Arg 0) (Arg 1)))
-  ;; => (Add (Const 1) (Const 2))
-  (function Subst{btype}All (VecOperand {btype}) {btype} :unextractable)
-
-  ;; helper that keeps track of how many arguments
-  ;; we have substituted so far
-  ;;       (vec of arguments, progress through that vec, and the expression to substitute into
-  (function Subst{btype}AllHelper (VecOperand i64 {btype}) {btype}  :unextractable)
-
-  (rewrite (Subst{btype}All arg-vec expr)
-           (Subst{btype}AllHelper arg-vec 0 expr)
-           :ruleset subst)
-
-  (rule
-    ((= helper (Subst{btype}AllHelper (VO arg-vec) progress expr))
-      (< progress (vec-length arg-vec)))
-    ((union helper
-            (Subst{btype}AllHelper
-              (VO arg-vec)
-              ;; increment progress
-              (+ progress 1)
-              ;; substitute the current argument into the expression
-              (Subst{btype} expr progress (vec-get arg-vec progress)))))
-      :ruleset subst)
-
-  ;; base case: we are done substituting
-  (rule
-    ((= helper (Subst{btype}AllHelper (VO arg-vec) progress expr))
-      (= progress (vec-length arg-vec)))
-    ((union helper expr))
-    :ruleset subst)
-  "
-    )
-}
-
-// "subst-beneath" rules support replacing a specific {Expr, Body, Operand,
-//  VecOperand, VecVecOperand} with another.
-//
-// - The key relations are (relation can-subst-TYPE-beneath (Body TYPE TYPE)),
-//   where TYPE is one of {Expr, Body, Operand, VecOperand, VecVecOperand}.
-// - Add (can-subst-TYPE-beneath above from to) if it is sound to replace zero
-//   or more occurrences of `from` with `to`, strictly within `above`.
-// - Then, saturate the subst ruleset for the appropriate unions to be made.
-//
-// Some intuition behind what is actually happening:
-//   If it's safe to replace `(badd a b)` with `to` in some context, then it's
-//   also safe to replace `(other-op (badd a b) c)` with `(other-op to c)` in
-//   the same context.
-//
-//   In this way, `can-subst-*-beneath` learns more possible replacements
-//   "bottom-up", then, when we reach the top of a context (a body), we union
-//   only at the top.
-//
-// See [src/rvsdg/tests.rs] for examples.
-//
-// rtjoa: We could parameterize on `above`'s type (currently, always Body) to
-// support substituting under a Function as well
+/// "subst-beneath" rules support replacing a specific {Expr, Body, Operand,
+///  VecOperand, VecVecOperand} with another.
+///
+/// - The key relations are (relation can-subst-TYPE-beneath (Body TYPE TYPE)),
+///   where TYPE is one of {Expr, Body, Operand, VecOperand, VecVecOperand}.
+/// - Add (can-subst-TYPE-beneath above from to) if it is sound to replace zero
+///   or more occurrences of `from` with `to`, strictly within `above`.
+/// - Then, saturate the subst ruleset for the appropriate unions to be made.
+///
+/// Some intuition behind what is actually happening:
+///   If it's safe to replace `(badd a b)` with `to` in some context, then it's
+///   also safe to replace `(other-op (badd a b) c)` with `(other-op to c)` in
+///   the same context.
+///
+///   In this way, `can-subst-*-beneath` learns more possible replacements
+///   "bottom-up", then, when we reach the top of a context (a body), we union
+///   only at the top.
+///
+/// See [src/rvsdg/tests.rs] for examples.
+///
+/// rtjoa: We could parameterize on `above`'s type (currently, always Body) to
+/// support substituting under a Function as well
 fn subst_beneath_rules() -> Vec<String> {
     let mut res = vec!["
         (relation can-subst-Expr-beneath (Body Expr Expr))
@@ -197,11 +153,6 @@ fn subst_beneath_rules() -> Vec<String> {
 
 pub(crate) fn subst_rules() -> String {
     let mut res = vec![include_str!("subst.egg").to_string()];
-
-    for btype in &["Expr", "Operand", "Body", "VecOperand", "VecVecOperand"] {
-        res.push(subst_all_rule(btype.to_string()));
-    }
-
     res.extend(subst_beneath_rules());
     res.join("\n")
 }

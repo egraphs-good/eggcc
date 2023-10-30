fdff05f1f44b09433d59b7745a1246e8b2d9f358
yzy <yzy2019@uw.edu>
2023-10-30 16:44:20 -0700

    intend to space

--- src/rvsdg/egglog_optimizer/loop_invariant.rs
+++ src/rvsdg/egglog_optimizer/loop_invariant.rs
@@ -1,15 +1,15 @@
 fn inv_binary_ops (bop: String) -> String {
-	format!(
-	"(rule ((find_inv_expr theta ({bop} ty a b)))
-	 		((find_inv_oprd theta a) (find_inv_oprd theta b)))
+    format!(
+    "(rule ((find_inv_expr theta ({bop} ty a b)))
+            ((find_inv_oprd theta a) (find_inv_oprd theta b)))
 
-	(rule ((is_inv_oprd body a) 
-			 (is_inv_oprd body b) 
-			 (find_inv_expr body expr)
-			 (= expr ({bop} a b)))
-		 ((is_inv_expr body expr)))
-	"
-	)
+    (rule ((is_inv_oprd body a) 
+             (is_inv_oprd body b) 
+             (find_inv_expr body expr)
+             (= expr ({bop} a b)))
+         ((is_inv_expr body expr)))
+    "
+    )
 }
 
 
@@ -17,7 +17,7 @@ pub(crate) fn loop_invariant() -> String {
     let mut res = vec![include_str!("loop_invariant.egg").to_string()];
 
     for bop in &["badd", "bsub", "bmul", "bfmul", "bdiv", "beq", 
-				"blt", "bgt", "ble", "bge", "bnot", "band", "bor"] {
+                "blt", "bgt", "ble", "bge", "bnot", "band", "bor"] {
         res.push(inv_binary_ops(bop.to_string()));
     }
 

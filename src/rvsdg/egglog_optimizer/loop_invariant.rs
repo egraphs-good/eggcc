00e84a4ca843a39b08da16b042dc52ab5faa6767
yzy <yzy2019@uw.edu>
2023-10-31 21:47:00 -0700

    add find_inv_expr for print and call

--- src/rvsdg/egglog_optimizer/loop_invariant.rs
+++ src/rvsdg/egglog_optimizer/loop_invariant.rs
@@ -1,6 +1,6 @@
-fn inv_binary_ops (bop: String) -> String {
+fn inv_binary_ops(bop: String) -> String {
     format!(
-    "(rule ((find_inv_expr theta ({bop} ty a b)))
+        "(rule ((find_inv_expr theta ({bop} ty a b)))
             ((find_inv_oprd theta a) (find_inv_oprd theta b)) :ruleset loop_inv_detect)
 
     (rule ((is_inv_oprd body a) 
@@ -12,16 +12,15 @@ fn inv_binary_ops (bop: String) -> String {
     )
 }
 
-
 pub(crate) fn loop_invariant_detection() -> String {
     let mut res = vec![include_str!("loop_invariant.egg").to_string()];
 
-    for bop in &["badd", "bsub", "bmul", "bfmul", "bdiv", "beq", 
-                "blt", "bgt", "ble", "bge", "bnot", "band", "bor"] {
+    for bop in &[
+        "badd", "bsub", "bmul", "bfmul", "bdiv", "beq", "blt", "bgt", "ble", "bge", "bnot", "band",
+        "bor",
+    ] {
         res.push(inv_binary_ops(bop.to_string()));
     }
 
     res.join("\n")
 }
-
-

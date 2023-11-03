b97074385fa23513f9485ddd73c5c952d7d61ef0
oflatt <oflatt@gmail.com>
2023-11-03 15:50:07 -0700

    it worked on loops!

--- src/rvsdg/egglog_optimizer/passthrough_optimize.rs
+++ src/rvsdg/egglog_optimizer/passthrough_optimize.rs
@@ -76,7 +76,8 @@ pub(crate) fn passthrough_optimize_rules() -> String {
 (rule
    (({ctor} vec))
    ((set (Extracted{vectype}Helper ({ctor} vec) 0)
-         ({vectype}AndCost ({ctor} (vec-of)) 0))))
+         ({vectype}AndCost ({ctor} (vec-of)) 0)))
+    :ruleset {ruleset})
 
 ;; extract one more thing
 (rule
@@ -87,7 +88,9 @@ pub(crate) fn passthrough_optimize_rules() -> String {
    ((set (Extracted{vectype}Helper ({ctor} vec) (+ index 1))
          ({vectype}AndCost
              ({ctor} (vec-push current expr))
-             (+ current-cost expr-cost)))))
+             (+ current-cost expr-cost))))
+    :ruleset {ruleset})
+    
 
             
 ;; finished extracting, create result
@@ -97,7 +100,8 @@ pub(crate) fn passthrough_optimize_rules() -> String {
    ;; at the end
    (= index (vec-length vec)))
   ((set (Extracted{vectype} ({ctor} vec))
-        result)))
+        result))
+  :ruleset {ruleset})
 
 
       "
@@ -121,6 +125,46 @@ pub(crate) fn passthrough_optimize_rules() -> String {
   :ruleset {ruleset})
 
 
+;; PureOp doesn't add cost
+(rule
+  ((= lhs (PureOp expr))
+   (= (ExprAndCost expr-extracted expr-cost)
+      (ExtractedExpr expr)))
+   ((set (ExtractedBody lhs) (BodyAndCost (PureOp expr-extracted) expr-cost)))
+    :ruleset {ruleset})
+
+;; Nor does Node
+(rule
+  ((= lhs (Node body))
+   (= (BodyAndCost body-extracted body-cost)
+      (ExtractedBody body)))
+   ((set (ExtractedOperand lhs) (OperandAndCost (Node body-extracted) body-cost)))
+    :ruleset {ruleset})
+
+;; Theta gets a cost of 1 for now
+(rule
+  ((= lhs (Theta pred inputs outputs))
+   (= (OperandAndCost pred-extracted pred-cost)
+      (ExtractedOperand pred))
+   (= (VecOperandAndCost inputs-extracted inputs-cost)
+      (ExtractedVecOperand inputs))
+   (= (VecOperandAndCost outputs-extracted outputs-cost)
+      (ExtractedVecOperand outputs)))
+   ((set (ExtractedBody lhs)
+         (BodyAndCost
+            (Theta pred-extracted inputs-extracted outputs-extracted)
+            (+ 1 (+ pred-cost (+ inputs-cost outputs-cost))))))
+    :ruleset {ruleset})
+
+;; Project is also free
+(rule ((= lhs (Project index body))
+       (= (BodyAndCost body-extracted body-cost)
+          (ExtractedBody body)))
+      ((set (ExtractedOperand lhs)
+            (OperandAndCost (Project index body-extracted) body-cost)))
+      :ruleset {ruleset})
+
+
 ;; Optimization! If a theta passes along argument,
 ;; can extract the input instead.
 (rule ((= lhs (Project index loop))
@@ -138,6 +182,7 @@ pub(crate) fn passthrough_optimize_rules() -> String {
           (ExtractedVecOperand body)))
       ((union func
               (Func name intypes outtypes extracted)))
+      :ruleset {ruleset}
       )
         "
     ));

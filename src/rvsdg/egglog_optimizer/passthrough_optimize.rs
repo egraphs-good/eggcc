1e985325d309d20baba291fe83a0ccb2dd03ec9d
oflatt <oflatt@gmail.com>
2023-11-07 16:14:29 -0800

    fix up error

--- src/rvsdg/egglog_optimizer/passthrough_optimize.rs
+++ src/rvsdg/egglog_optimizer/passthrough_optimize.rs
@@ -9,16 +9,17 @@ pub(crate) fn passthrough_optimize_rules() -> String {
 ;; can union with the inputs.
 ;; BUT only if the theta is pure!
 (rule ((= lhs (Project index loop))
-        (= loop (Theta pred inputs outputs))
-        (= (VecOperand-get outputs index) (Arg index))
-        (= input (VecOperand-get inputs index))
-        (Body-is-pure loop)
+       (= loop (Theta pred inputs outputs))
+       (= (VecOperand-get outputs index) (Arg index))
+       (= passed-through (VecOperand-get inputs index))
+       (Body-is-pure loop)
       )
-      ((union lhs input))
+      ((union lhs passed-through))
       :ruleset {ruleset})
 
 ;; If a gamma passes along an argument in both branches,
-;; extract the input instead.
+;; union project with input
+;; BUT only if the gamma is pure!
 (rule ((= lhs (Project index loop))
        (= loop (Gamma pred inputs outputs))
        (= outputs (VVO outputs-inner))
@@ -27,8 +28,8 @@ pub(crate) fn passthrough_optimize_rules() -> String {
        (= outputs1 (VecVecOperand-get outputs 1))
        (= (VecOperand-get outputs0 index) (Arg index))
        (= (VecOperand-get outputs1 index) (Arg index))
-       (= passedthrough (ExtractedOperand (VecOperand-get inputs index))))
-      ((set (ExtractedOperand lhs) passedthrough))
+       (= passed-through (VecOperand-get inputs index)))
+      ((union lhs passed-through))
       :ruleset {ruleset})
         "
     ));

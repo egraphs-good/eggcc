2c971bf04c73cf27175b0100a009ff600e0ec7f4
oflatt <oflatt@gmail.com>
2023-11-28 15:47:31 -0800

    run id analysis, misc fixes

--- uniqueargs/src/main.rs
+++ uniqueargs/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let program = vec![
         // header
         include_str!("schema.egg"),
+        include_str!("id_analysis.egg"),
         include_str!("deep_copy.egg"),
         // optimizations
         include_str!("switch_rewrites.egg"),

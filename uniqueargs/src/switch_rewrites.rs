9dece0db67bb46deb826601a3422ba89ac4f5dec
Alex Fischman <alexdfischman@gmail.com>
2023-12-01 11:19:32 -0800

    Add tests, fix deepcopy bug

--- uniqueargs/src/switch_rewrites.rs
+++ uniqueargs/src/switch_rewrites.rs
@@ -3,23 +3,66 @@ pub use crate::*;
 #[test]
 fn switch_rewrite_and() -> Result {
     let build = "
-        (let test_a_id (id (i64-fresh!)))
-        (let test_a (Switch test_a_id
-                            (TODO)
-                            (TODO)
-                            (TODO)))
-
-        (let test_b_id (id (i64-fresh!)))
-        (let test_b (Switch test_b_id
-                            (TODO)
-                            (TODO)
-                            (TODO)))
+        (let test-a-id (id (i64-fresh!)))
+        (let test-a (Switch
+            test-a-id
+            (band (Bool global-id false) (Bool global-id true))
+            (EVec (vec-of))
+            (EVec (vec-of
+                (EVec (vec-of (Num test-a-id 2)))
+                (EVec (vec-of (Num test-a-id 1)))))))
     ";
-    let check = "(check (= test_a test_b))";
+    let check = "
+        (check (= test-a
+            (Switch
+                test-b-id
+                (Bool global-id false)
+                (EVec (vec-of (Bool global-id true)))
+                (EVec (vec-of
+                    (EVec (vec-of (Num test-b-id 2)))
+                    (EVec (vec-of (Project (Switch
+                        test-c-id
+                        (Arg test-b-id 0)
+                        (EVec (vec-of))
+                        (EVec (vec-of
+                            (EVec (vec-of (Num test-c-id 2)))
+                            (EVec (vec-of (Num test-c-id 1)))))
+                    ) 0)))))
+            )))
+    ";
     run_test(build, check)
 }
 
 #[test]
 fn switch_rewrite_or() -> Result {
-    todo!()
+    let build = "
+        (let test-a-id (id (i64-fresh!)))
+        (let test-a (Switch
+            test-a-id
+            (bor (Bool global-id false) (Bool global-id true))
+            (EVec (vec-of))
+            (EVec (vec-of
+                (EVec (vec-of (Num test-a-id 2)))
+                (EVec (vec-of (Num test-a-id 1)))))))
+    ";
+    let check = "
+        (check (= test-a
+            (Switch
+                test-b-id
+                (Bool global-id false)
+                (EVec (vec-of (Bool global-id true)))
+                (EVec (vec-of
+                    (EVec (vec-of (Project (Switch
+                        test-c-id
+                        (Arg test-b-id 0)
+                        (EVec (vec-of))
+                        (EVec (vec-of
+                            (EVec (vec-of (Num test-c-id 2)))
+                            (EVec (vec-of (Num test-c-id 1)))))
+                    ) 0)))
+                    (EVec (vec-of (Num test-b-id 1)))
+                ))
+            )))
+    ";
+    run_test(build, check)
 }

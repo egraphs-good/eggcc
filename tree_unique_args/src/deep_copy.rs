ce08d35c9a29656dc613cc22ccd919528d930c7a
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2023-12-29 19:58:35 -0800

    fix deepcopy tests

--- tree_unique_args/src/deep_copy.rs
+++ tree_unique_args/src/deep_copy.rs
@@ -1,41 +1,26 @@
-use std::iter;
-
 use crate::ir::{Constructor, ESort, Purpose};
 use strum::IntoEnumIterator;
 
 fn deep_copy_rule_for_ctor(ctor: Constructor) -> String {
-    // e.g. "Add x y"
-    let ctor_pattern = iter::once(ctor.name())
-        .chain(ctor.fields().iter().map(|field| field.name))
-        .collect::<Vec<_>>()
-        .join(" ");
+    // e.g. "(Add x y)"
+    let ctor_pattern = ctor.construct(|field| field.name.to_string());
 
-    // e.g. ["(DeepCopyExpr x new-id)", "(DeepCopyExpr y new-id)"]
-    let substed_fields = ctor
-        .fields()
-        .iter()
-        .map(|field| match field.purpose {
-            Purpose::CapturingId => "new-inner-id".to_string(),
-            Purpose::Static(_) => field.name.to_string(),
-            Purpose::CapturedExpr => {
-                let var = field.name;
-                let sort = field.sort().name();
-                format!("(DeepCopy{sort} {var} new-inner-id)")
-            }
-            Purpose::ReferencingId => "new-id".to_string(),
-            Purpose::SubExpr | Purpose::SubListExpr => {
-                let var = field.name;
-                let sort = field.sort().name();
-                format!("(DeepCopy{sort} {var} new-id)")
-            }
-        })
-        .collect::<Vec<_>>();
-
-    // e.g. "Add (DeepCopyExpr x new-id) (DeepCopyExpr y new-id)"
-    let copied_ctor = iter::once(ctor.name().to_string())
-        .chain(substed_fields.into_iter())
-        .collect::<Vec<_>>()
-        .join(" ");
+    // e.g. "(Add (DeepCopyExpr x new-id)", "(DeepCopyExpr y new-id))"
+    let copied_ctor = ctor.construct(|field| match field.purpose {
+        Purpose::CapturingId => "new-inner-id".to_string(),
+        Purpose::Static(_) => field.name.to_string(),
+        Purpose::CapturedExpr => {
+            let var = field.name;
+            let sort = field.sort().name();
+            format!("(DeepCopy{sort} {var} new-inner-id)")
+        }
+        Purpose::ReferencingId => "new-id".to_string(),
+        Purpose::SubExpr | Purpose::SubListExpr => {
+            let var = field.name;
+            let sort = field.sort().name();
+            format!("(DeepCopy{sort} {var} new-id)")
+        }
+    });
 
     let sort = ctor.sort().name();
     let br = "\n      ";
@@ -44,12 +29,12 @@ fn deep_copy_rule_for_ctor(ctor: Constructor) -> String {
         .iter()
         .any(|field| field.purpose == Purpose::CapturingId);
     let actions = if creates_context {
-        format!("(let new-inner-id (i64-fresh!)){br} (union e ({copied_ctor}))")
+        format!("(let new-inner-id (i64-fresh!)){br} (union e {copied_ctor})")
     } else {
-        format!("(union e ({copied_ctor}))")
+        format!("(union e {copied_ctor})")
     };
     format!(
-        "(rule ((= e (DeepCopy{sort} ({ctor_pattern}) new-id))){br}({actions}){br}:ruleset always-run)"
+        "(rule ((= e (DeepCopy{sort} {ctor_pattern} new-id))){br}({actions}){br}:ruleset always-run)"
     )
 }
 

b7c9289336b717bffd9b401340bbf96a728b7283
Alex Fischman <alexdfischman@gmail.com>
2024-01-10 15:21:33 -0800

    Add switch rewrite tests

--- tree_unique_args/src/switch_rewrites.rs
+++ tree_unique_args/src/switch_rewrites.rs
@@ -16,3 +16,35 @@ pub(crate) fn egglog() -> String {
 "
     )
 }
+
+#[test]
+fn switch_rewrite_and() -> crate::Result {
+    let build = "
+(let id (Id (i64-fresh!)))
+(let switch (Switch (And (Boolean id false) (Boolean id true))
+                    (Pair (Num id 1) (Num id 2))))
+    ";
+    let check = "
+(check (= switch (Switch (Boolean id false)
+                         (Pair (Switch (Boolean id true)
+                                       (Pair (Num id 1) (Num id 2)))
+                               (Num id 2)))))
+    ";
+    crate::run_test(build, check)
+}
+
+#[test]
+fn switch_rewrite_or() -> crate::Result {
+    let build = "
+(let id (Id (i64-fresh!)))
+(let switch (Switch (Or (Boolean id false) (Boolean id true))
+                    (Pair (Num id 1) (Num id 2))))
+    ";
+    let check = "
+(check (= switch (Switch (Boolean id false)
+                         (Pair (Num id 1)
+                               (Switch (Boolean id true)
+                                       (Pair (Num id 1) (Num id 2)))))))
+    ";
+    crate::run_test(build, check)
+}

8fdc675c59e10daa68cdde99f640fbe45f631438
oflatt <oflatt@gmail.com>
2024-01-15 10:56:49 -0700

    working on loop unroll without helper

--- tree_unique_args/src/main.rs
+++ tree_unique_args/src/main.rs
@@ -9,6 +9,7 @@ pub(crate) mod conditional_invariant_code_motion;
 pub(crate) mod deep_copy;
 pub(crate) mod function_inlining;
 pub(crate) mod ir;
+pub(crate) mod loop_unrolling;
 pub(crate) mod purity_analysis;
 pub(crate) mod subst;
 pub(crate) mod switch_rewrites;

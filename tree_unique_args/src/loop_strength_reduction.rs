65d24b2e089d8376fa55e524bca3096b67550f71
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2024-01-23 14:22:47 -0800

    Add loop strength reduction test for mult by a num

--- tree_unique_args/src/loop_strength_reduction.rs
+++ tree_unique_args/src/loop_strength_reduction.rs
@@ -1,5 +1,6 @@
+// Checks that loop strength reduction works for repeated multiplication by a passthrough arg
 #[test]
-fn loop_strength_reduction() -> Result<(), egglog::Error> {
+fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
     let build = "
         (let outer-id (Id (i64-fresh!)))
         (let loop-id (Id (i64-fresh!)))
@@ -71,6 +72,86 @@ fn loop_strength_reduction() -> Result<(), egglog::Error> {
                                     (Num whatever-id 1)
                                   ))
                             (Nil)))))) (Nil))
+                            ))
+            )
+    ))
+    ";
+
+    crate::run_test(build, check)
+}
+
+// Checks that loop strength reduction works for repeated multiplication by a Number
+#[test]
+fn loop_strength_reduction_num_const() -> Result<(), egglog::Error> {
+    let build = "
+        (let outer-id (Id (i64-fresh!)))
+        (let loop-id (Id (i64-fresh!)))
+        (let input-list
+                (Cons (Num outer-id 0) ; a
+                (Cons (Num outer-id 0) (Nil))) ; i
+        )
+        (let inputs
+            (
+                All
+                (Parallel)
+                input-list
+            )
+        )
+        (let pred
+            (LessThan (Get (Arg loop-id) 1) (Num loop-id 4))
+        )
+        (let output-list
+            (Cons (Mul (Num loop-id 5) (Get (Arg loop-id) 1)) ; i * c
+            (Cons (Add (Get (Arg loop-id) 1) (Num loop-id 1)) (Nil))) ; i += 1
+        )
+        (let loop
+            (
+                Loop
+                loop-id
+                inputs
+                (All
+                    (Sequential)
+                    (Pair
+                        pred
+                        (All
+                            (Parallel)
+                            output-list
+                        )
+                    )
+                )
+            )
+        )
+        (ExprIsValid loop)
+    ";
+
+    let check = "
+        (check (Inv loop (Num outer-id 5) (Num loop-id 5)))
+        (query-extract :variants 3 loop)
+        (check (
+            =
+            loop
+            (Loop
+                whatever-id
+                (All
+                    (Parallel)
+                    (Cons (Num outer-id 0) ; a
+                    (Cons (Num outer-id 0) ; i
+                    (Cons
+                        (Mul (Num outer-id 5) (Num outer-id 0))
+                        (Nil)))))
+                (All
+                    (Sequential)
+                    (Cons
+                        (LessThan (Get (Arg whatever-id) 1) (Num whatever-id 4))
+                        (Cons (All
+                            (Parallel)
+                            (Cons (Get (Arg whatever-id) 2) ; i * c => d
+                            (Cons (Add (Get (Arg whatever-id) 1) (Num whatever-id 1)) ; i += 1
+                            (Cons 
+                                (Add (Get (Arg whatever-id) 2) (Mul (Num whatever-id 5)
+                                    (Num whatever-id 1)
+                                  ))
+                            (Nil))))) (Nil))
                             ))
             )
     ))

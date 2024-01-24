d070fc80d9f5b9d736e46fa8d0df1ffcd4d2fbd4
Anjali Pal <anjalip@cs.washington.edu>
2024-01-23 22:20:40 -0800

    add context + let

--- tree_unique_args/src/type_analysis.rs
+++ tree_unique_args/src/type_analysis.rs
@@ -1,49 +1,50 @@
 #[test]
 fn simple_types() -> Result<(), egglog::Error> {
     let build = "
-        (let id1 (Id (i64-fresh!)))
-        (let id2 (Id (i64-fresh!)))
-        (let n (Add (Num id1 1) (Num id2 2)))
+        (let id (Id (i64-fresh!)))
+        (let n (Add (Num id 1) (Num id 2)))
         (let m (Mul n n))
         (let s (Sub n m))
         (let x (LessThan m n))
         (let y (Not x))
         (let z (And x (Or y y)))
-        (HasTypeDemand s)
-        (HasTypeDemand z)
+        (HasTypeDemand id s)
+        (HasTypeDemand id z)
         ";
     let check = "
     (run-schedule (saturate type-analysis))
 
-    (check (HasType n (IntT)))
-    (check (HasType m (IntT)))
-    (check (HasType s (IntT)))
-    (check (HasType x (BoolT)))
-    (check (HasType y (BoolT)))
-    (check (HasType z (BoolT)))
+    (check (HasType id n (IntT)))
+    (check (HasType id m (IntT)))
+    (check (HasType id s (IntT)))
+    (check (HasType id x (BoolT)))
+    (check (HasType id y (BoolT)))
+    (check (HasType id z (BoolT)))
     ";
     crate::run_test(build, check)
 }
 
+
 #[test]
 fn switch_boolean() -> Result<(), egglog::Error> {
     let build = "
-  (let b1 (Boolean (Id (i64-fresh!)) true))
-  (let n1 (Num (Id (i64-fresh!)) 1))
-  (let n2 (Num (Id (i64-fresh!)) 3))
+  (let id (Id (i64-fresh!)))
+  (let b1 (Boolean id true))
+  (let n1 (Num id 1))
+  (let n2 (Num id 3))
   (let switch
     (Switch (Not (LessThan n1 n2))
             (Cons (Add n1 n1) (Cons (Sub n1 n2) (Nil)))))
   (let wrong_switch
     (Switch b1 (Cons n1 (Cons n2 (Cons n1 (Nil))))))
-  (HasTypeDemand switch)
-  (HasTypeDemand wrong_switch)
+  (HasTypeDemand id switch)
+  (HasTypeDemand id wrong_switch)
   ";
     let check = "
   (run-schedule (saturate type-analysis))
 
-  (check (HasType switch (IntT)))
-  (fail (check (HasType wrong_switch ty))) ; should not be able to type a boolean swith with 3 cases
+  (check (HasType id switch (IntT)))
+  (fail (check (HasType id wrong_switch ty))) ; should not be able to type a boolean swith with 3 cases
   ";
     crate::run_test(build, check)
 }
@@ -51,10 +52,11 @@ fn switch_boolean() -> Result<(), egglog::Error> {
 #[test]
 fn switch_int() -> Result<(), egglog::Error> {
     let build = "
-  (let n1 (Num (Id (i64-fresh!)) 1))
-  (let n2 (Num (Id (i64-fresh!)) 2))
-  (let n3 (Num (Id (i64-fresh!)) 3))
-  (let n4 (Num (Id (i64-fresh!)) 4))
+  (let id (Id (i64-fresh!)))
+  (let n1 (Num id 1))
+  (let n2 (Num id 2))
+  (let n3 (Num id 3))
+  (let n4 (Num id 4))
   (let s1
     (Switch n1
             (Cons (Add n1 n1) (Cons (Sub n1 n2) (Nil)))))
@@ -62,16 +64,16 @@ fn switch_int() -> Result<(), egglog::Error> {
     (Switch (Mul n1 n2) (Cons (LessThan n3 n4) (Nil))))
   (let s3
     (Switch (Sub n2 n2) (Cons (Print n1) (Cons (Print n4) (Cons (Print n3) (Nil))))))  
-  (HasTypeDemand s1)
-  (HasTypeDemand s2)
-  (HasTypeDemand s3)
+  (HasTypeDemand id s1)
+  (HasTypeDemand id s2)
+  (HasTypeDemand id s3)
   ";
     let check = "
   (run-schedule (saturate type-analysis))
 
-  (check (HasType s1 (IntT)))
-  (check (HasType s2 (BoolT)))
-  (check (HasType s3 (UnitT)))
+  (check (HasType id s1 (IntT)))
+  (check (HasType id s2 (BoolT)))
+  (check (HasType id s3 (UnitT)))
   ";
     crate::run_test(build, check)
 }
@@ -79,7 +81,8 @@ fn switch_int() -> Result<(), egglog::Error> {
 #[test]
 fn tuple() -> Result<(), egglog::Error> {
     let build = "
-  (let n (Add (Num (Id (i64-fresh!)) 1) (Num (Id (i64-fresh!)) 2)))
+  (let id (Id (i64-fresh!)))
+  (let n (Add (Num id 1) (Num id 2)))
         (let m (Mul n n))
         (let s (Sub n m))
         (let x (LessThan m n))
@@ -90,32 +93,43 @@ fn tuple() -> Result<(), egglog::Error> {
   (let tup2 (All (Sequential) (Cons z (Nil))))
   (let tup3 (All (Parallel) (Cons x (Cons m (Nil)))))
   (let tup4 (All (Parallel) (Cons tup2 (Cons tup3 (Nil)))))
-  (HasTypeDemand tup1)
-  (HasTypeDemand tup2)
-  (HasTypeDemand tup3)
-  (HasTypeDemand tup4)
+  (HasTypeDemand id tup1)
+  (HasTypeDemand id tup2)
+  (HasTypeDemand id tup3)
+  (HasTypeDemand id tup4)
 
   (let get1 (Get tup3 0))
   (let get2 (Get tup3 1))
   (let get3 (Get (Get tup4 1) 1))
-  (HasTypeDemand get1)
-  (HasTypeDemand get2)
-  (HasTypeDemand get3)
+  (HasTypeDemand id get1)
+  (HasTypeDemand id get2)
+  (HasTypeDemand id get3)
   ";
     let check = "
   (run-schedule (saturate type-analysis))
-  (check (HasType tup1 (TupleT (TNil))))
-  (check (HasType tup2 (TupleT (TCons (BoolT) (TNil)))))
-  (check (HasType tup3 (TupleT (TCons (BoolT) (TCons (IntT) (TNil))))))
-  (check (HasType tup4
+  (check (HasType id tup1 (TupleT (TNil))))
+  (check (HasType id tup2 (TupleT (TCons (BoolT) (TNil)))))
+  (check (HasType id tup3 (TupleT (TCons (BoolT) (TCons (IntT) (TNil))))))
+  (check (HasType id tup4
     (TupleT (TCons (TupleT (TCons (BoolT) (TNil)))
     (TCons (TupleT (TCons (BoolT) (TCons (IntT) (TNil))))
           (TNil))))))
 
   
-  (check (HasType get1 (BoolT)))
-  (check (HasType get2 (IntT)))
-  (check (HasType get3 (IntT)))
+  (check (HasType id get1 (BoolT)))
+  (check (HasType id get2 (IntT)))
+  (check (HasType id get3 (IntT)))
   ";
     crate::run_test(build, check)
+}
+
+#[test]
+fn lets() -> Result<(), egglog::Error> {
+  let build = "
+    (let let-id (Id (i64-fresh!)))
+    (let outer-ctx (Id (i64-fresh!)))
+    (let l (Let let-id (Num outer-ctx 5) (Add (Arg let-id) (Arg let-id))))
+  ";
+  let check = "";
+  crate::run_test(build, check)
 }

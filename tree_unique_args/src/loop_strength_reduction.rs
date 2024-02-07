4f9b22a35bc7145caed9581f46e1dd3033acdcd9
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2024-02-06 20:23:35 -0800

    Loop strength reduction modified - syntax errors fixed, tests WIP

--- tree_unique_args/src/loop_strength_reduction.rs
+++ tree_unique_args/src/loop_strength_reduction.rs
@@ -12,6 +12,7 @@ fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
         (let inputs
             (
                 All
+                outer-id
                 (Parallel)
                 input-list
             )
@@ -30,10 +31,12 @@ fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
                 loop-id
                 inputs
                 (All
+                    loop-id
                     (Sequential)
                     (Pair
                         pred
                         (All
+                            loop-id
                             (Parallel)
                             output-list
                         )
@@ -44,13 +47,27 @@ fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
         (ExprIsValid loop)
     ";
 
+    "
+    (Let
+        (Id 2)
+        (All
+            (Id 0)
+            (Parallel)
+            (Pair (Arg (Id 0)) (Num (Id 0) 0)))
+            (Let
+                (Id 3)
+                (Loop whatever-id
+                    (All (Id 2) (Parallel) (Cons (Num (Id 2) 0) (Cons (Get (Arg (Id 2)) 1) (Cons (Num (Id 2) 3) (Cons (Mul (Num (Id 2) 3) (Num (Id 2) 0)) (Nil))))))
+                    (All whatever-id (Sequential) (Cons (LessThan (Get (Arg whatever-id) 1) (Num whatever-id 4)) (Cons (All whatever-id (Parallel) (Cons (Get (Arg whatever-id) 3) (Cons (Add (Get (Arg whatever-id) 1) (Num whatever-id 1)) (Cons (Get (Arg whatever-id) 2) (Cons (Add (Get (Arg whatever-id) 3) (Mul (Get (Arg whatever-id) 2) (Num whatever-id 1))) (Nil)))))) (Nil)))))
+                (All (Id 3) (Parallel) (Cons (Get (Arg (Id 3)) 0) (Cons (Get (Arg (Id 3)) 1) (Cons (Get (Arg (Id 3)) 2) (Nil)))))))
+    ";
     let check = "
-        (check (
-            =
+    (check (=
             loop
             (Loop
                 whatever-id
                 (All
+                    outer-id
                     (Parallel)
                     (Cons (Num outer-id 0) ; a
                     (Cons (Num outer-id 0) ; i
@@ -59,10 +76,12 @@ fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
                         (Mul (Num outer-id 3) (Num outer-id 0))
                         (Nil))))))
                 (All
+                    whatever-id
                     (Sequential)
                     (Cons
                         (LessThan (Get (Arg whatever-id) 1) (Num whatever-id 4))
                         (Cons (All
+                            whatever-id
                             (Parallel)
                             (Cons (Get (Arg whatever-id) 3) ; i * c => d
                             (Cons (Add (Get (Arg whatever-id) 1) (Num whatever-id 1)) ; i += 1
@@ -71,10 +90,7 @@ fn loop_strength_reduction_passthrough_const() -> Result<(), egglog::Error> {
                                 (Add (Get (Arg whatever-id) 3) (Mul (Get (Arg whatever-id) 2)
                                     (Num whatever-id 1)
                                   ))
-                            (Nil)))))) (Nil))
-                            ))
-            )
-    ))
+                            (Nil)))))) (Nil)))))))
     ";
 
     crate::run_test(build, check)
@@ -93,6 +109,7 @@ fn loop_strength_reduction_num_const() -> Result<(), egglog::Error> {
         (let inputs
             (
                 All
+                outer-id
                 (Parallel)
                 input-list
             )
@@ -110,10 +127,12 @@ fn loop_strength_reduction_num_const() -> Result<(), egglog::Error> {
                 loop-id
                 inputs
                 (All
+                    loop-id
                     (Sequential)
                     (Pair
                         pred
                         (All
+                            loop-id
                             (Parallel)
                             output-list
                         )
@@ -125,14 +144,13 @@ fn loop_strength_reduction_num_const() -> Result<(), egglog::Error> {
     ";
 
     let check = "
-        (check (Inv loop (Num outer-id 5) (Num loop-id 5)))
-        (query-extract :variants 3 loop)
         (check (
             =
             loop
             (Loop
                 whatever-id
                 (All
+                    outer-id
                     (Parallel)
                     (Cons (Num outer-id 0) ; a
                     (Cons (Num outer-id 0) ; i
@@ -140,10 +158,12 @@ fn loop_strength_reduction_num_const() -> Result<(), egglog::Error> {
                         (Mul (Num outer-id 5) (Num outer-id 0))
                         (Nil)))))
                 (All
+                    whatever-id
                     (Sequential)
                     (Cons
                         (LessThan (Get (Arg whatever-id) 1) (Num whatever-id 4))
                         (Cons (All
+                            whatever-id
                             (Parallel)
                             (Cons (Get (Arg whatever-id) 2) ; i * c => d
                             (Cons (Add (Get (Arg whatever-id) 1) (Num whatever-id 1)) ; i += 1

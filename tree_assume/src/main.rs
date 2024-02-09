a701cd3e9c3888e8faa665f89210f843c402c710
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2024-02-08 23:18:06 -0800

    dump on failure, factor out prologue

--- tree_assume/src/main.rs
+++ tree_assume/src/main.rs
@@ -1,7 +1,5 @@
-use tree_assume::egglog_test;
-use tree_assume::interpreter::Value;
-use tree_assume::Result;
+use tree_assume::prologue;
 
-fn main() -> Result {
-    egglog_test("", "", vec![], Value::Tuple(vec![]), Value::Tuple(vec![]))
+fn main() -> () {
+    println!("{}", prologue());
 }

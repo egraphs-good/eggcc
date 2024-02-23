660cf8fdc3ed5cef071ef065ae5a96aab41a368e
Alex Fischman <alexdfischman@gmail.com>
2024-02-22 23:33:56 -0800

    Interpreter short-circuiting bugfix and working on 3/4 purity test

--- tree_in_context/src/optimizations/switch_rewrites.rs
+++ tree_in_context/src/optimizations/switch_rewrites.rs
@@ -43,37 +43,37 @@ fn switch_rewrite_three_quarters_or() -> crate::Result {
     )
 }
 
-// #[test]
-// fn switch_rewrite_purity() -> crate::Result {
-//     let build = "
-// (let switch-id (Id (i64-fresh!)))
-// (let let-id (Id (i64-fresh!)))
-// (let impure (Let let-id (All switch-id (Parallel) (Nil)) (All let-id (Sequential) (Pair (Boolean let-id true) (Print (Num let-id 1))))))
-// (let switch (Switch (And (Boolean switch-id false) (Get impure 0))
-//                     (Pair (Num switch-id 1) (Num switch-id 2))))
-// (ExprIsValid switch)
-//     ";
-//     let check = "
-// (fail (check (= switch (Switch (Boolean switch-id false)
-//                                (Pair (Num switch-id 1)
-//                                      (Switch (Get impure 0)
-//                                              (Pair (Num switch-id 1) (Num switch-id 2))))))))
-//     ";
-//     crate::run_test(build, check)?;
+#[test]
+fn switch_rewrite_three_quarters_purity() -> crate::Result {
+    use crate::ast::*;
 
-//     let build = "
-// (let switch-id (Id (i64-fresh!)))
-// (let let-id (Id (i64-fresh!)))
-// (let pure   (Let let-id (All switch-id (Parallel) (Nil)) (All let-id (Sequential) (Cons (Boolean let-id true) (Nil)))))
-// (let switch (Switch (And (Boolean switch-id false) (Get pure 0))
-//                     (Pair (Num switch-id 1) (Num switch-id 2))))
-// (ExprIsValid switch)
-//     ";
-//     let check = "
-// (check (= switch (Switch (Boolean switch-id false)
-//                          (Pair (Num switch-id 1)
-//                                (Switch (Get pure 0)
-//                                        (Pair (Num switch-id 1) (Num switch-id 2)))))))
-//     ";
-//     crate::run_test(build, check)
-// }
+    let pure = get(single(ttrue()), 0);
+
+    let build = tif(and(tfalse(), pure.clone()), int(1), int(2));
+
+    let check = tif(tfalse(), tif(pure, int(1), int(2)), int(2));
+
+    egglog_test(
+        &format!("{build}"),
+        &format!("(check (= {build} {check}))"),
+        vec![build.to_program(emptyt(), intt())],
+        val_empty(),
+        val_int(2),
+        vec![],
+    )?;
+
+    let impure = get(concat_par(tprint(int(1)), single(ttrue())), 0);
+
+    let build = tif(and(tfalse(), impure.clone()), int(1), int(2));
+
+    let check = tif(tfalse(), tif(impure, int(1), int(2)), int(2));
+
+    egglog_test(
+        &format!("{build}"),
+        &format!("(check (!= {build} {check}))"),
+        vec![build.to_program(emptyt(), intt())],
+        val_empty(),
+        val_int(2),
+        vec!["1".to_string()],
+    )
+}

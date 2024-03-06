47d1782f846a01eb9e2a231c00c21b020c86e0b1
oflatt <oflatt@gmail.com>
2024-03-05 16:15:20 -0800

    more progress on converting ops, add state edge

--- src/rvsdg/from_tree.rs
+++ src/rvsdg/from_tree.rs
@@ -1,7 +1,9 @@
 //! Convert tree programs to RVSDGs
 
-use bril_rs::{ConstOps, Literal};
-use tree_in_context::schema::{BaseType, Expr, RcExpr, TreeProgram, Type};
+use std::iter;
+
+use bril_rs::{ConstOps, EffectOps, Literal, ValueOps};
+use tree_in_context::schema::{BaseType, BinaryOp, Expr, RcExpr, TreeProgram, Type, UnaryOp};
 
 use super::{BasicExpr, Operand, RvsdgBody, RvsdgFunction, RvsdgProgram, RvsdgType};
 
@@ -9,6 +11,7 @@ type Operands = Vec<Operand>;
 
 struct TreeToRvsdg {
     nodes: Vec<RvsdgBody>,
+    current_state_edge: Operand,
 }
 
 pub(crate) fn tree_to_rvsdg(tree: TreeProgram) -> RvsdgProgram {
@@ -35,46 +38,113 @@ fn bril_type_from_type(ty: Type) -> bril_rs::Type {
     }
 }
 
-fn rvsdg_types_from_tuple_type(ty: Type) -> Vec<RvsdgType> {
-    let Type::TupleT(tys) = ty else {
-        panic!("Expected tuple type in tree_type_to_rvsdg_types")
-    };
-    tys.into_iter()
-        .map(|ty| RvsdgType::Bril(bril_type_from_type(ty)))
-        .collect()
-}
-
 fn tree_func_to_rvsdg(func: RcExpr) -> RvsdgFunction {
-    let types = func.func_output_ty().expect("Expected function types");
+    let Type::TupleT(output_types) = func.func_output_ty().expect("Expected function types") else {
+        panic!("Expected tuple type in tree_func_to_rvsdg")
+    };
 
-    let mut converter = TreeToRvsdg { nodes: vec![] };
+    let Type::TupleT(input_types) = func.func_input_ty().expect("Expected function types") else {
+        panic!("Expected tuple type for inputs in tree_func_to_rvsdg")
+    };
+
+    // initial state edge is last argument
+    let state_edge = Operand::Arg(output_types.len());
+    let mut converter = TreeToRvsdg {
+        nodes: vec![],
+        current_state_edge: state_edge,
+    };
+
+    let output_rvsdg_types: Vec<RvsdgType> = output_types
+        .into_iter()
+        .map(|ty| RvsdgType::Bril(bril_type_from_type(ty)))
+        .chain(iter::once(RvsdgType::PrintState))
+        .collect();
 
     let converted = converter.convert_func(func.clone());
+
+    assert_eq!(
+        converted.len(),
+        output_rvsdg_types.len(),
+        "Expected same number of results as output types"
+    );
 
     RvsdgFunction {
         name: func
             .func_name()
             .expect("Expected function in tree_func_to_rvsdg"),
-        args: rvsdg_types_from_tuple_type(types),
+        // normal types and a state edge at the end
+        args: input_types
+            .into_iter()
+            .map(|ty| RvsdgType::Bril(bril_type_from_type(ty)))
+            .chain(iter::once(RvsdgType::PrintState))
+            .collect(),
         nodes: converter.nodes,
-        results: converted,
+        results: output_rvsdg_types.into_iter().zip(converted).collect(),
+    }
+}
+
+fn value_op_from_binary_op(bop: BinaryOp) -> Option<ValueOps> {
+    match bop {
+        BinaryOp::Add => Some(ValueOps::Add),
+        BinaryOp::Sub => Some(ValueOps::Sub),
+        BinaryOp::Mul => Some(ValueOps::Mul),
+        BinaryOp::Div => Some(ValueOps::Div),
+        BinaryOp::And => Some(ValueOps::And),
+        BinaryOp::Or => Some(ValueOps::Or),
+        BinaryOp::Eq => Some(ValueOps::Eq),
+        BinaryOp::LessThan => Some(ValueOps::Lt),
+        BinaryOp::GreaterThan => Some(ValueOps::Gt),
+        BinaryOp::Write => None,
+        BinaryOp::PtrAdd => Some(ValueOps::PtrAdd),
+    }
+}
+
+fn effect_op_from_binary_op(bop: BinaryOp) -> Option<EffectOps> {
+    match bop {
+        BinaryOp::Write => Some(EffectOps::Store),
+        _ => None,
+    }
+}
+
+fn value_op_from_unary_op(uop: UnaryOp) -> Option<ValueOps> {
+    match uop {
+        UnaryOp::Not => Some(ValueOps::Not),
+        UnaryOp::Print => None,
+        UnaryOp::Load => Some(ValueOps::Load),
+    }
+}
+
+fn effect_op_from_unary_op(uop: UnaryOp) -> Option<EffectOps> {
+    match uop {
+        UnaryOp::Not => None,
+        UnaryOp::Print => Some(EffectOps::Print),
+        UnaryOp::Load => None,
     }
 }
 
 impl TreeToRvsdg {
-    pub fn convert_func(&mut self, func: RcExpr) -> Vec<(RvsdgType, Operand)> {
+    pub fn convert_func(&mut self, func: RcExpr) -> Vec<Operand> {
         todo!()
     }
 
     fn push_basic(&mut self, basic: BasicExpr<Operand>) -> Vec<Operand> {
-        let new_id = self.nodes.len();
-        self.nodes.push(RvsdgBody::BasicOp(basic));
-        vec![Operand::Project(0, new_id)]
+        if let BasicExpr::Effect(effect, mut operands) = basic {
+            let new_id = self.nodes.len();
+            operands.push(self.current_state_edge.clone());
+            self.nodes
+                .push(RvsdgBody::BasicOp(BasicExpr::Effect(effect, operands)));
+            self.current_state_edge = Operand::Project(1, new_id);
+            vec![Operand::Project(0, new_id)]
+        } else {
+            let new_id = self.nodes.len();
+            self.nodes.push(RvsdgBody::BasicOp(basic));
+            vec![Operand::Project(0, new_id)]
+        }
     }
 
     fn convert_expr(&mut self, expr: RcExpr) -> Operands {
         match expr.as_ref() {
-            Expr::Const(constant) => match constant {
+            Expr::Const(constant, ty) => match constant {
                 tree_in_context::schema::Constant::Int(integer) => self.push_basic(
                     BasicExpr::Const(ConstOps::Const, Literal::Int(*integer), bril_rs::Type::Int),
                 ),
@@ -91,7 +161,48 @@ impl TreeToRvsdg {
                 let r = self.convert_expr(r.clone());
                 let l = l[0].clone();
                 let r = r[0].clone();
-                self.push_basic(BasicExpr::Op(*op, vec![l, r], bril_rs::Type::Int))
+                if let Some(vop) = value_op_from_binary_op(*op) {
+                    self.push_basic(BasicExpr::Op(vop, vec![l, r], bril_rs::Type::Int))
+                } else if let Some(eop) = effect_op_from_binary_op(*op) {
+                    self.push_basic(BasicExpr::Effect(eop, vec![l, r]))
+                } else {
+                    panic!("Unknown binary op {:?}", op)
+                }
+            }
+            Expr::Uop(op, child) => {
+                let child = self.convert_expr(child.clone());
+                let child = child[0].clone();
+                if let Some(vop) = value_op_from_unary_op(*op) {
+                    self.push_basic(BasicExpr::Op(vop, vec![child], bril_rs::Type::Int))
+                } else if let Some(eop) = effect_op_from_unary_op(*op) {
+                    self.push_basic(BasicExpr::Effect(eop, vec![child]))
+                } else {
+                    panic!("Unknown unary op {:?}", op)
+                }
+            }
+            Expr::Get(child, index) => {
+                let child = self.convert_expr(child.clone());
+                assert!(child.len() > *index, "Index out of bounds");
+                vec![child[*index].clone()]
+            }
+            Expr::Alloc(size, ty) => {
+                let size = self.convert_expr(size.clone());
+                let size = size[0].clone();
+                self.push_basic(BasicExpr::Op(
+                    ValueOps::Alloc,
+                    vec![size],
+                    bril_type_from_type(ty.clone()),
+                ))
+            }
+            Expr::Arg(ty) => {
+                let Type::TupleT(tys) = ty.clone() else {
+                    panic!("Expected tuple type in tree_type_to_rvsdg_types")
+                };
+                // One operand for each argument
+                tys.iter()
+                    .enumerate()
+                    .map(|(i, _ty)| Operand::Arg(i))
+                    .collect()
             }
         }
     }

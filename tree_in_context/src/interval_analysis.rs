745c86e392aa1ceaa038fccf768c75254882b8be
Anjali Pal <anjalip@cs.washington.edu>
2024-03-22 12:19:20 -0700

    conditionals

--- tree_in_context/src/interval_analysis.rs
+++ tree_in_context/src/interval_analysis.rs
@@ -84,3 +84,66 @@ fn test_add_constant_fold() -> crate::Result {
         vec![],
     )
 }
+
+#[test]
+fn test_add_interval() -> crate::Result {
+    let e = add(int(3), int(4)).with_arg_types(emptyt(), base(intt()));
+    int_interval_test(e, base(intt()), val_empty(), val_int(7), 7, 7)
+}
+
+#[test]
+fn test_lt_interval() -> crate::Result {
+    let e = less_than(int(2), int(3)).with_arg_types(emptyt(), base(boolt()));
+    bool_interval_test(e, base(boolt()), val_empty(), val_bool(true), true, true)
+}
+
+#[test]
+fn test_if_constant_fold() -> crate::Result {
+    let c = less_than(int(2), int(3)).with_arg_types(emptyt(), base(boolt()));
+    let e = tif(c, int_ty(3, emptyt()), int_ty(4, emptyt()));
+
+    int_interval_test(e, base(intt()), val_empty(), val_int(3), 3, 3)
+}
+
+#[test]
+fn if_interval() -> crate::Result {
+    let e = tif(
+        less_than(iarg(), int_ty(3, base(intt()))),
+        int_ty(4, base(intt())),
+        int_ty(5, base(intt())),
+    );
+    let f = function("main", base(intt()), base(intt()), e.clone()).func_with_arg_types();
+
+    egglog_test(
+        &format!("{f}"),
+        &format!("(check (ival {e}) (IntI 4 5))"),
+        vec![f.to_program(base(intt()), base(intt()))],
+        val_int(1),
+        val_int(4),
+        vec![],
+    )
+}
+
+#[test]
+fn nested_if() -> crate::Result {
+    let inner = tif(
+        less_than(iarg(), int_ty(3, base(intt()))),
+        int_ty(4, base(intt())),
+        int_ty(5, base(intt())),
+    );
+    let outer = tif(
+        less_eq(inner.clone(), int_ty(10, base(intt()))),
+        int_ty(20, base(intt())),
+        int_ty(30, base(intt())),
+    );
+    let f = function("main", base(intt()), base(intt()), outer.clone()).func_with_arg_types();
+
+    egglog_test(
+        &format!("{f}"),
+        &format!("(check (ival {inner}) (IntI 4 5)) (check (ival {outer}) (IntI 20 20))"),
+        vec![f.to_program(base(intt()), base(intt()))],
+        val_int(2),
+        val_int(20),
+        vec![],
+    )
+}

bb1ddc6d1f2d9a2eb2c47cc81ce73cac12c5bb91
zhiyuan yan <yanzhiyuan2019@gmail.com>
2024-04-03 16:09:29 -0700

    boundary

--- dag_in_context/src/optimizations/loop_invariant.rs
+++ dag_in_context/src/optimizations/loop_invariant.rs
@@ -80,36 +80,90 @@ fn is_invariant_rule_for_ctor(ctor: Constructor) -> Option<String> {
     }
 }
 
+fn boundary_for_ctor(ctor: Constructor) -> Option<String> {
+    let ruleset = " :ruleset boundary-analysis";
+
+    match ctor {
+        // Ops with one SubExpr should not be boundary, except effects
+        // ListExpr handled separately
+        Constructor::Cons
+        | Constructor::Nil
+        | Constructor::Arg
+        | Constructor::Get
+        | Constructor::Function => None,
+        _ => {
+            let ctor_pattern = ctor.construct(|field| field.var());
+            let res = ctor
+                .filter_map_fields(|field| {
+                    let var = field.var();
+                    match field.purpose {
+                        Purpose::SubExpr => Some(format!(
+                            "
+(rule ((= true (is-inv-Expr loop expr1)) 
+       (= false (is-inv-Expr loop expr2)) 
+       (= expr2 {ctor_pattern}) 
+       (= expr1 {var})) 
+       ((boundary-Expr loop expr1)){ruleset})"
+                        )),
+                        _ => None,
+                    }
+                })
+                .join("\n");
+            Some(res)
+        }
+    }
+}
+
 pub(crate) fn rules() -> Vec<String> {
     iter::once(include_str!("loop_invariant.egg").to_string())
         .chain(Constructor::iter().filter_map(is_inv_base_case_for_ctor))
         .chain(Constructor::iter().filter_map(is_invariant_rule_for_ctor))
+        .chain(Constructor::iter().filter_map(boundary_for_ctor))
         .collect::<Vec<_>>()
 }
 
 #[test]
-fn test_invariant_detect_simple() -> crate::Result {
+fn test_invariant_detect() -> crate::Result {
     use crate::ast::*;
-    let output_ty = tuplet!(intt(), intt(), intt(), intt());
-    let inv = sub(getat(2), getat(1)).with_arg_types(output_ty.clone(), base(intt()));
+    let output_ty = tuplet!(intt(), intt(), intt(), intt(), statet());
+    let inner_inv = sub(getat(2), getat(1)).with_arg_types(output_ty.clone(), base(intt()));
+    let inv = add(inner_inv.clone(), int(0)).with_arg_types(output_ty.clone(), base(intt()));
     let pred = less_than(getat(0), getat(3)).with_arg_types(output_ty.clone(), base(boolt()));
     let not_inv = add(getat(0), inv.clone()).with_arg_types(output_ty.clone(), base(intt()));
-    let add_inv = add(inv.clone(), int(4)).with_arg_types(output_ty.clone(), base(intt()));
+    let inv_in_print = add(inv.clone(), int_ty(4, output_ty.clone()));
+    let print =
+        tprint(inv_in_print.clone(), getat(4)).with_arg_types(output_ty.clone(), base(statet()));
+
     let my_loop = dowhile(
-        parallel!(int(1), int(2), int(3), int(4)),
-        concat(
-            parallel!(pred.clone(), not_inv.clone(), getat(1),),
-            parallel!(getat(2), get(parallel!(add_inv.clone(), getat(3)), 1),),
+        parallel!(int(1), int(2), int(3), int(4), getat(0)),
+        concat_par(
+            parallel!(pred.clone(), not_inv.clone(), getat(1)),
+            concat_par(parallel!(getat(2), getat(3)), single(print.clone())),
         ),
     )
-    .with_arg_types(emptyt(), output_ty.clone());
+    .with_arg_types(tuplet!(statet()), output_ty.clone());
 
-    let build = format!("(let loop {})", my_loop);
+    let build = format!(
+        "(let loop {})
+        (let inv {})
+        (let inv_in_print {})
+        (let pred {})
+        (let not_inv {})
+        (let print {})
+        (let inner_inv {})",
+        my_loop, inv, inv_in_print, pred, not_inv, print, inner_inv
+    );
     let check = format!(
-        "(check (= true (is-inv-Expr loop {inv})))
-         (check (= true (is-inv-Expr loop {add_inv})))
-         (check (= false (is-inv-Expr loop {pred})))
-         (check (= false (is-inv-Expr loop {not_inv})))",
+        "(check (= true (is-inv-Expr loop inv)))
+		(check (= true (is-inv-Expr loop inv_in_print)))
+		(check (= false (is-inv-Expr loop pred)))
+		(check (= false (is-inv-Expr loop not_inv)))
+        (check (boundary-Expr loop inv))
+        (check (boundary-Expr loop inv_in_print))
+        (fail (check (boundary-Expr loop not_inv)))
+        (fail (check (boundary-Expr loop pred)))
+        (check (= true (is-inv-Expr loop inner_inv)))
+        (fail (check (boundary-Expr loop inner_inv)))"
     );
 
     egglog_test(

3c64a99e68f76226e1a4603a291a1409216fd7d9
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2024-04-05 12:56:59 -0700

    Fix ContextOf base case bug; add test cases

--- dag_in_context/src/utility/context_of.rs
+++ dag_in_context/src/utility/context_of.rs
@@ -48,17 +48,15 @@ pub(crate) fn rules() -> Vec<String> {
         // Base case: InContext means ContextOf
         "
 (rule ((InContext ctx e))
-    ((ContextOf e ctx)) :ruleset always-run)"
+    ((ContextOf (InContext ctx e) ctx)) :ruleset always-run)"
             .to_string(),
         // Error checking - each expr should only have a single context
         "
-        (relation TwoContexts (Expr Assumption Assumption))
 (rule ((ContextOf x ctx1)
         (ContextOf x ctx2)
         (!= ctx1 ctx2))
     (
-        (TwoContexts x ctx1 ctx2)
-        ;panic \"Equivalent expressions have nonequivalent context\"
+        (panic \"Equivalent expressions have nonequivalent context\")
     )
     :ruleset error-checking)"
             .to_string(),
@@ -92,6 +90,69 @@ fn test_context_of() -> crate::Result {
     // Don't pass in any programs, just want build/check to be checked
     crate::egglog_test(
         &format!("(let build {build})"),
+        &check,
+        vec![],
+        val_empty(),
+        val_empty(),
+        vec![],
+    )
+}
+
+// Check that InContext means ContextOf
+#[test]
+fn test_context_of_base_case() -> crate::Result {
+    let build = "(InContext (InFunc \"somefunc\") (Const (Int 5) (Base (IntT))))";
+    let check = "(ContextOf (InContext (InFunc \"somefunc\") (Const (Int 5) (Base (IntT)))) (InFunc \"somefunc\"))";
+
+    crate::egglog_test(
+        &format!("(let build {build})"),
+        check,
+        vec![],
+        crate::ast::val_empty(),
+        crate::ast::val_empty(),
+        vec![],
+    )
+}
+
+#[test]
+#[should_panic]
+fn test_context_of_panics_if_two() {
+    let build = "
+        (let ctx1 (InFunc \"main\"))
+        (let ctx2 (InFunc \"notmain\"))
+        (let conflict-expr (Bop (And) (InContext ctx1 (Const (Bool false) (Base (BoolT)))) (InContext ctx2 (Const (Bool true) (Base (BoolT))))))";
+    let check = "";
+
+    let _ = crate::egglog_test(
+        build,
+        check,
+        vec![],
+        crate::ast::val_empty(),
+        crate::ast::val_empty(),
+        vec![],
+    );
+}
+
+// Functions should not have a context
+#[test]
+fn test_context_of_no_func_context() -> crate::Result {
+    use crate::ast::*;
+
+    let build = function(
+        "main",
+        emptyt(),
+        base(intt()),
+        dowhile(
+            single(int_ty(5, base(intt()))),
+            add(get(arg_ty(base(intt())), 0), int_ty(4, base(intt()))),
+        ),
+    )
+    .func_add_context();
+
+    let check = format!("(fail (check (ContextOf {} ctx)))", build.clone());
+
+    crate::egglog_test(
+        &build.to_string(),
         &check,
         vec![],
         val_empty(),

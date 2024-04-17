4c587c53195a290a179722486ae92566480a5c5a
Alex Fischman <alexdfischman@gmail.com>
2024-04-17 16:57:28 -0700

    Delete rust test, won't work with contexts

--- dag_in_context/src/optimizations/mod.rs
+++ dag_in_context/src/optimizations/mod.rs
@@ -3,7 +3,6 @@ pub mod conditional_invariant_code_motion;
 pub mod function_inlining;
 pub mod is_valid;
 pub mod loop_invariant;
-pub mod loop_splitting;
 pub mod passthrough;
 pub mod purity_analysis;
 pub mod switch_rewrites;

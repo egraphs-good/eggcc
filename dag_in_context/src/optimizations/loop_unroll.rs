5d92197ea5ccb7f7d92cc66eaa2861fb8f72b550
oflatt <oflatt@gmail.com>
2024-04-24 11:07:08 -0700

    loop unroll test

---
+++ dag_in_context/src/optimizations/loop_unroll.rs
@@ -0,0 +1,30 @@
+#[test]
+fn loop_unroll_once() -> crate::Result {
+    use crate::ast::*;
+    use crate::egglog_test;
+    let prog = dowhile(
+        parallel!(int(1)),
+        parallel!(tfalse(), add(getat(0), int(1))),
+    )
+    .with_arg_types(base(intt()), tuplet!(intt()))
+    .initialize_ctx();
+
+    let expected = parallel!(int(2))
+        .with_arg_types(base(intt()), tuplet!(intt()))
+        .initialize_ctx();
+
+    egglog_test(
+        &format!("{prog}"),
+        &format!(
+            "
+(check (= {prog} {expected}))"
+        ),
+        vec![
+            prog.to_program(base(intt()), tuplet!(intt())),
+            expected.to_program(base(intt()), tuplet!(intt())),
+        ],
+        intv(0),
+        tuplev!(intv(2)),
+        vec![],
+    )
+}

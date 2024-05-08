7446f0b73bf233084d08c04e4bd0c6ad659d7d2d
Ryan Berger <ryanbberger@gmail.com>
2024-05-08 10:38:35 -0700

    [wip] refactor linecount to give detailed linecount numbers

--- infra/gen_linecount.py
+++ infra/gen_linecount.py
@@ -9,24 +9,34 @@ def get_generated_egg():
     return len(program.stdout.splitlines())
 
 def get_written_egg():
-    total_lines = 0
     file_paths = glob.glob("./**/*.egg", recursive=True)
+
+    counts = {}
     for file_path in file_paths:
         with open(file_path, 'r') as file:
+            if file_path not in counts:
+                counts[file_path] = 0
             lines = file.readlines()
             total_lines += len(lines)
-    return total_lines
+    return counts
 
 def get_rust_lines():
     rust_lines_output = subprocess.run(["tokei", "--output", "json", "./"], capture_output=True)
     return json.loads(rust_lines_output.stdout)["Rust"]["code"]
+
 
 def gen_linecount_table():
     rust_lines = str(get_rust_lines())
     written_egg = str(get_written_egg())
     generated_egg = str(get_generated_egg())
 
-    fmt = """\\begin{tabular}{ |s|p{2cm}| }
+    counts = get_written_egg()
+    total = 0
+    for _, v in counts:
+        total += v
+
+
+    overview = """\\begin{tabular}{ |s|p{2cm}| }
 \hline
 \multicolumn{2}{|c|}{Line Counts} \\\
 \hline
@@ -36,6 +46,7 @@ Rust & %s \\\\
 Written Egg & %s \\\\
 Generated EGG & %s \\\\
 \hline
-\end{tabular}""" % (rust_lines, written_egg, generated_egg)
-    return fmt
+\end{tabular}""" % (rust_lines, written_egg, total)
+
+    return overview
 

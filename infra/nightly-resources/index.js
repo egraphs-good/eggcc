0e0d9083c8f4536ec1ec69181a4d01e588feeff3
Anjali Pal <anjalip@cs.washington.edu>
2024-05-13 13:39:07 -0700

    prettier

--- infra/nightly-resources/index.js
+++ infra/nightly-resources/index.js
@@ -189,7 +189,7 @@ function buildEntry(baseline, current) {
   const results = current.hyperfine.results[0];
   const baselineResults = baseline?.hyperfine.results[0];
 
-  const result =  {
+  const result = {
     name: current.runMethod,
     mean: { class: "", value: tryRound(results.mean) },
     meanVsBaseline: diffAttribute(results, baselineResults, "mean"),
@@ -273,13 +273,13 @@ function refreshView() {
     return 0;
   });
 
-  parsed.forEach(benchmark => {
-    benchmark.executions.data.forEach(run => {
+  parsed.forEach((benchmark) => {
+    benchmark.executions.data.forEach((run) => {
       if (run.llvm) {
-        run.name = `<a target="_blank" rel="noopener noreferrer" href="llvm.html?benchmark=${benchmark.name}&runmode=${run.name}">${run.name}</a>`
+        run.name = `<a target="_blank" rel="noopener noreferrer" href="llvm.html?benchmark=${benchmark.name}&runmode=${run.name}">${run.name}</a>`;
       }
-    })
-  })
+    });
+  });
 
   document.getElementById("profile").innerHTML = ConvertJsonToTable(parsed);
 

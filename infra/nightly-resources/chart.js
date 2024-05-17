6d2350ca31559557ca4187096f37a988c7de1ed8
Anjali Pal <anjalip@cs.washington.edu>
2024-05-17 10:16:26 -0700

    sort by mode on click

--- infra/nightly-resources/chart.js
+++ infra/nightly-resources/chart.js
@@ -25,32 +25,44 @@ function getDataForBenchmarkRunMode(benchmark, runMode) {
   }
 }
 
-function parseDataForChart() {
-  const benchmarks = Array.from(GLOBAL_DATA.enabledBenchmarks).sort();
+function parseDataForChart(sortByMode) {
+  const modes = GLOBAL_DATA.enabledModes;
+  const benchmarks = GLOBAL_DATA.enabledBenchmarks;
+  let sortedBenchmarks = Array.from(benchmarks).sort();
+  const data = {}
+  modes.forEach(mode => {
+    data[mode] = {};
+    benchmarks.forEach(benchmark => {
+      const entry = getDataForBenchmarkRunMode(benchmark, mode);
+      if (entry) {
+        data[mode][benchmark] = {
+          mode: mode,
+          benchmark: benchmark,
+          mean: entry.hyperfine.results[0].mean,
+          stddev: entry.hyperfine.results[0].stddev,
+        };
+      }
+    });
+    if (mode === sortByMode) {
+      sortedBenchmarks = Object.values(data[mode]).sort((a, b) => b.mean - a.mean).map(x => x.benchmark);
+    }
+  });
   const datasets = {};
-  GLOBAL_DATA.enabledModes.forEach((mode) => {
+  modes.forEach(mode => {
     datasets[mode] = {
       label: mode,
       backgroundColor: COLORS[mode],
       data: Array(benchmarks.length).fill(0),
       borderWidth: 1,
-      errorBars: {},
-    };
-  });
-  benchmarks.forEach((benchName, idx) => {
-    GLOBAL_DATA.enabledModes.forEach((mode) => {
-      const benchDataForMode = getDataForBenchmarkRunMode(benchName, mode);
-      if (benchDataForMode) {
-        const mean = benchDataForMode.hyperfine.results[0].mean;
-        const stddev = benchDataForMode.hyperfine.results[0].stddev;
-        datasets[mode].data[idx] = mean;
-        datasets[mode].errorBars[benchName] = { plus: stddev, minus: stddev };
-      } else {
-        console.warn(`no data for ${benchName} ${mode}`);
-      }
+      errorBars: {}
+    }
+    Object.values(data[mode]).forEach(point => {
+      const idx = sortedBenchmarks.indexOf(point.benchmark);
+      datasets[mode].data[idx] = point.mean;
+      datasets[mode].errorBars[point.benchmark] = {plus: point.stddev, minus: point.stddev}
     });
-  });
-  return { labels: benchmarks, datasets: Object.values(datasets) };
+  })
+  return {labels: Array.from(sortedBenchmarks), datasets: Object.values(datasets)}
 }
 
 function initializeChart() {
@@ -67,7 +79,11 @@ function initializeChart() {
           beginAtZero: true,
         },
       },
+      legend: {
+        onClick: (_, item) => refreshChart(item.text)
+      },
       plugins: {
+        
         chartJsPluginErrorBars: {
           color: "black",
         },
@@ -78,10 +94,10 @@ function initializeChart() {
 
 // Seems important for the charting library to change the data but not
 // create a new chart to avoid some weird rendering flicekrs.
-function refreshChart() {
+function refreshChart(sortByMode) {
   if (!GLOBAL_DATA.chart) {
     return;
   }
-  GLOBAL_DATA.chart.data = parseDataForChart();
+  GLOBAL_DATA.chart.data = parseDataForChart(sortByMode);
   GLOBAL_DATA.chart.update();
 }

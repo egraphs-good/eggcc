f4c6ef9187c9847c77f343c2ce98686beb270317
Anjali Pal <anjalip@cs.washington.edu>
2024-05-17 10:57:10 -0700

    speedup chart

--- infra/nightly-resources/chart.js
+++ infra/nightly-resources/chart.js
@@ -25,12 +25,28 @@ function getDataForBenchmarkRunMode(benchmark, runMode) {
   }
 }
 
+function getValue(entry) {
+  if (GLOBAL_DATA.chartMode === "absolute") {
+    return entry.mean;
+  } else if (GLOBAL_DATA.chartMode === "speedup") {
+    return entry.speedup;
+  } else {
+    throw new Error(`unknown chart mode ${GLOBAL_DATA.chartMode}`);
+  }
+}
+
 function parseDataForChart(sortByMode) {
-  const modes = GLOBAL_DATA.enabledModes;
+  const BASELINE_MODE = "llvm-peep";
+  let sortedModes = Array.from(GLOBAL_DATA.enabledModes).filter(
+    (x) => x !== BASELINE_MODE,
+  );
+  sortedModes.unshift(BASELINE_MODE);
+
   const benchmarks = GLOBAL_DATA.enabledBenchmarks;
   let sortedBenchmarks = Array.from(benchmarks).sort();
+
   const data = {};
-  modes.forEach((mode) => {
+  sortedModes.forEach((mode) => {
     data[mode] = {};
     benchmarks.forEach((benchmark) => {
       const entry = getDataForBenchmarkRunMode(benchmark, mode);
@@ -41,16 +57,27 @@ function parseDataForChart(sortByMode) {
           mean: entry.hyperfine.results[0].mean,
           stddev: entry.hyperfine.results[0].stddev,
         };
+        if (mode === BASELINE_MODE) {
+          data[mode][benchmark].speedup = 1;
+        } else {
+          const baseline = data[BASELINE_MODE][benchmark];
+          if (baseline) {
+            data[mode][benchmark].speedup =
+              baseline.mean / data[mode][benchmark].mean;
+          } else {
+            console.warn(`No speedup baseline for ${benchmark}`);
+          }
+        }
       }
     });
     if (mode === sortByMode) {
       sortedBenchmarks = Object.values(data[mode])
-        .sort((a, b) => b.mean - a.mean)
+        .sort((a, b) => getValue(b) - getValue(a))
         .map((x) => x.benchmark);
     }
   });
   const datasets = {};
-  modes.forEach((mode) => {
+  GLOBAL_DATA.enabledModes.forEach((mode) => {
     datasets[mode] = {
       label: mode,
       backgroundColor: COLORS[mode],
@@ -60,11 +87,14 @@ function parseDataForChart(sortByMode) {
     };
     Object.values(data[mode]).forEach((point) => {
       const idx = sortedBenchmarks.indexOf(point.benchmark);
-      datasets[mode].data[idx] = point.mean;
-      datasets[mode].errorBars[point.benchmark] = {
-        plus: point.stddev,
-        minus: point.stddev,
-      };
+
+      datasets[mode].data[idx] = getValue(point);
+      if (GLOBAL_DATA.chartMode === "absolute") {
+        datasets[mode].errorBars[point.benchmark] = {
+          plus: point.stddev,
+          minus: point.stddev,
+        };
+      }
     });
   });
   return {

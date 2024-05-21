3fe434c8d92f8a9e87a9ca5ad55383873aaa8b7e
Anjali Pal <anjalip@cs.washington.edu>
2024-05-21 15:34:32 -0700

    nightly benchmark latex

--- infra/nightly-resources/latex.js
+++ infra/nightly-resources/latex.js
@@ -1,3 +1,10 @@
+function construct(pre, rows, post) {
+  return String.raw`
+  ${pre}
+  ${rows.join("\n")}
+  ${post}`;
+}
+
 function generateLineCountTable(latexData) {
   const writtenEggTotal = Object.values(latexData.written_egg).reduce(
     (acc, v) => acc + v,
@@ -33,14 +40,43 @@ function generateDetailedLineCountTable(latexData) {
   const post = String.raw`
   \hline
   \end{tabular}`;
-  return String.raw`
-  ${pre}
-  ${rows.join("\n")}
-  ${post}`;
+  return construct(pre, rows, post);
+}
+
+function generateRowsForBench(benchmark) {
+  const dataForBench = GLOBAL_DATA.currentRun.filter(x => x.benchmark === benchmark);
+  const rows = dataForBench.map((entry, idx) => {
+    const {mean, max, min, stddev} = entry.hyperfine.results[0];
+    const fstCol = idx === 0 ? String.raw`\multirow{${dataForBench.length}}{*}{${benchmark}}` : '';
+    return [
+      String.raw`\multicolumn{1}{|l|}{${fstCol}} &`,
+      String.raw`\multicolumn{1}{l|}{${entry.runMethod}}  &`,
+      String.raw`\multicolumn{1}{l|}{${tryRound(mean)}} &`,
+      String.raw`\multicolumn{1}{l|}{${tryRound(max)}} &`,
+      String.raw`\multicolumn{1}{l|}{${tryRound(min)}} &`,
+      String.raw`${tryRound(stddev)} \\`
+    ].join(" ");
+  });
+  return construct("", rows, String.raw` \hline`);
 }
 
 function generateBenchmarksTable(latexData) {
-  console.log(latexData);
+  const pre = String.raw`
+  \begin{table}[]
+  \resizebox{\textwidth}{!}{%
+  \begin{tabular}{|llllll|}
+  \hline
+  \multicolumn{6}{|c|}{Benchmarks} \\ \hline
+  \multicolumn{1}{|l|}{Name} & \multicolumn{5}{c|}{Executions} \\ \hline
+  \multicolumn{1}{|l|}{} & \multicolumn{1}{l|}{Run Method}  & \multicolumn{1}{l|}{Mean} & \multicolumn{1}{l|}{Max} & \multicolumn{1}{l|}{Min} & Std Dev \\ \hline`
+
+  const benchmarks = [...new Set(GLOBAL_DATA.currentRun.map(x => x.benchmark))].sort();
+  const rows = benchmarks.map(bench => generateRowsForBench(bench));
+
+  const post = String.raw`\end{tabular}%
+  }
+  \end{table}`
+  return construct(pre, rows, post);
 }
 
 function generateLatex(latexData, tableName) {

a2c4aa7093b7692d14048cffab994c434cdfef26
Anjali Pal <anjalip@cs.washington.edu>
2024-10-25 14:04:33 -0700

    checkpoint - working on select.rs

---
+++ tests/passing/small/select.rs
@@ -0,0 +1,16 @@
+// ARGS: 20
+fn main(x: i64) {
+    let ten: i64 = 10;
+    let done: i64 = ten;
+    let i: i64 = 0;
+    let res: i64 = 0;
+    while !(done == 5) {
+        i += 1;
+        res += i;
+        // done = ten;
+        if i == x {
+            done = 5;
+        }
+    }
+    println!("{}", res);
+}

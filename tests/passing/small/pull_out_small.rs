fffc0992ad8324a055da1ad23a41423ed0333467
Anjali Pal <anjalip@cs.washington.edu>
2025-01-23 11:23:21 -0800

    update test

--- tests/passing/small/pull_out_small.rs
+++ tests/passing/small/pull_out_small.rs
@@ -2,11 +2,11 @@
 fn main(x: i64) {
     let a: i64 = x * 5;
     if (x > 0) {
-        let r: i64 = a + x;
-        let z: i64 = 10;
+        let r: i64 = x * 5;
+        let z: i64 = 20;
         println!("{}", z);
     } else {
-        let r: i64 = x + a;
+        let r: i64 = x * 5;
         let z: i64 = 20;
         println!("{}", z);
     }

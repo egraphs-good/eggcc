65fca07b1c0feb2df61abce9668ae41cedcd22bf
Anjali Pal <anjalip@cs.washington.edu>
2025-02-03 12:27:49 -0800

    oops

--- tests/passing/small/select.rs
+++ tests/passing/small/select.rs
@@ -1,16 +1,15 @@
 // ARGS: 20
 fn main(x: i64) {
-    let res: i64 = x / 2;
-    // let ten: i64 = 10;
-    // let done: i64 = ten;
-    // let i: i64 = 0;
-    // let res: i64 = 0;
-    // while !(done == 5) {
-    //     i += 1;
-    //     res += i;
-    //     if i == x {
-    //         done = 5;
-    //     }
-    // }
+    let ten: i64 = 10;
+    let done: i64 = ten;
+    let i: i64 = 0;
+    let res: i64 = 0;
+    while !(done == 5) {
+        i += 1;
+        res += i;
+        if i == x {
+            done = 5;
+        }
+    }
     println!("{}", res);
 }

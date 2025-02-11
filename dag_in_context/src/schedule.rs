9bb557e47189a7dcf18e8b2ea6256e85c3adc1b7
Ryan Tjoa <ryan.j.tjoa@gmail.com>
2025-02-11 18:01:30 -0500

    fix rebase

--- dag_in_context/src/schedule.rs
+++ dag_in_context/src/schedule.rs
@@ -57,7 +57,7 @@ pub(crate) fn helpers() -> String {
      (saturate
        terms-helpers
        (saturate terms-helpers-helpers)))
-    (saturate memsimple)
+    (saturate mem-simple)
     ;; memory-helpers TODO run memory helpers for memory optimizations
 
     ;; finally, subsume now that helpers are done
